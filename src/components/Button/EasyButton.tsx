68f0e71e30cdf3cf0e2a35b327d351e5623841e1
abura1han <xbura1han@gmail.com>
2022-11-15 23:56:47 +0600

    Add: easy themes

--- src/components/Button/EasyButton.tsx
+++ src/components/Button/EasyButton.tsx
@@ -22,12 +22,27 @@ export interface ButtonProps {
  */
 export type ButtonTheme =
   | "custom"
-  | "error"
-  | "success"
-  | "warn"
-  | "purple"
-  | "pink"
-  | "primary";
+  | "easy_success"
+  | "easy_info"
+  | "easy_warn"
+  | "easy_error"
+  | "easy_purple"
+  | "primary_success"
+  | "primary_info"
+  | "primary_warn"
+  | "primary_error"
+  | "secondary_success"
+  | "secondary_info"
+  | "secondary_warn"
+  | "secondary_error"
+  | "primary_success_outline"
+  | "primary_info_outline"
+  | "primary_warn_outline"
+  | "primary_error_outline"
+  | "secondary_success_outline"
+  | "secondary_info_outline"
+  | "secondary_warn_outline"
+  | "secondary_error_outline";
 
 /**
  * Button default styles
@@ -46,33 +61,79 @@ const defaultStyles: React.CSSProperties = {
   boxShadow: "0px 2px 4px rgba(193, 193, 193, 0.25)",
 };
 
+/**
+ * Button styles obj contains normal, hover & other values
+ */
 const buttonStyles = {
-  error: {
+  easy_success: {
     normal: {
       ...defaultStyles,
-      color: "#FE0000",
-      backgroundColor: "#FFD9D9",
+      color: "#04A100",
+      backgroundColor: "#DAFFD9",
+      opacity: 1,
     },
     hover: {
       ...defaultStyles,
+      color: "#04A100",
+      backgroundColor: "#DAFFD9",
       opacity: 0.7,
     },
   },
-  purple: {
-    normal: {
-      ...defaultStyles,
-      color: "#8D00FF",
-      backgroundColor: "#EED9FF",
-    },
-    hover: defaultStyles,
-  },
-  primary: {
+  easy_info: {
     normal: {
       ...defaultStyles,
       color: "#006AFE",
       backgroundColor: "#D9E9FF",
+      opacity: 1,
     },
-    hover: defaultStyles,
+    hover: {
+      ...defaultStyles,
+      color: "#006AFE",
+      backgroundColor: "#D9E9FF",
+      opacity: 1,
+    },
+  },
+  easy_warn: {
+    normal: {
+      ...defaultStyles,
+      color: "#A48000",
+      backgroundColor: "#FFFBD9",
+      opacity: 1,
+    },
+    hover: {
+      ...defaultStyles,
+      color: "#A48000",
+      backgroundColor: "#FFFBD9",
+      opacity: 0.7,
+    },
+  },
+  easy_error: {
+    normal: {
+      ...defaultStyles,
+      color: "#FF0000",
+      backgroundColor: "#A48000",
+      opacity: 1,
+    },
+    hover: {
+      ...defaultStyles,
+      color: "#FF0000",
+      backgroundColor: "#A48000",
+      opacity: 0.7,
+    },
+  },
+  easy_purple: {
+    normal: {
+      ...defaultStyles,
+      color: "#8D00FF",
+      backgroundColor: "#EED9FF",
+      opacity: 1,
+    },
+    hover: {
+      ...defaultStyles,
+      color: "#8D00FF",
+      backgroundColor: "#EED9FF",
+      opacity: 0.7,
+    },
   },
 };
 
